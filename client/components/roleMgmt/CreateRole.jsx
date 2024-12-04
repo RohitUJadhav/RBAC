30c9384ae12a31e2932a5d1d9d03d165e88384ce
Rohit Jadhav <97385473+RohitUJadhav@users.noreply.github.com>
2024-12-04 16:55:37 +0530

    Update CreateRole.jsx

--- client/components/roleMgmt/CreateRole.jsx
+++ client/components/roleMgmt/CreateRole.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import RUsers from "./rUsers";
+import RUsers from "./RUsers";
 
 function CreateRole() {
 const[role,setRole] = useState()
